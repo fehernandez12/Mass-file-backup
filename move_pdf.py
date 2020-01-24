356f24a71773ff7a2a1bbb083ec9e9c32c6548a0
Felipe Hernández <feehernandezba@gmail.com>
2020-01-24 17:06:17 -0500

    Add files via upload
    Initial commit

---
+++ move_pdf.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import shutil
+import pathlib
+
+def _menu():
+    print('-----------------------------')
+    print('------ PDF Transporter ------')
+    print('-----------------------------')
+    print('Selecciona una opción: ')
+    print('[M]over archivos')
+    print('[S]alir')
+    opcion = input().upper()
+    _switch_case(opcion)
+
+def _solicitar_ruta_entrada():
+    ruta_entrada = input('Ingresa la lista de archivos a transportar:\n')
+    return ruta_entrada
+
+def _convertir_archivo_en_lista(r):
+    with open(r) as archivo:
+        arr = [line.rstrip('\n') for line in archivo]
+    return arr
+
+def _solicitar_path():
+    path = input('Ingresa el path en el que deseas replicar la estructura de carpetas:\n')
+    return path
+
+def _rutas_digester(x, y):
+    y = y[:-1]
+    for i in range(len(x)):
+        x[i] = x[i][2::]
+    for i in range(len(x)):
+        x[i] = y + x[i]
+    return x
+
+def _crear_directorios(arr):
+    arreglo_dir = []
+    for item in arr:
+        arr_item = item.split('/')
+        arr_item.pop(-1)
+        nuevo_directorio = ''
+        for i in arr_item:
+            nuevo_directorio += i
+            nuevo_directorio += '/'
+        arreglo_dir.append(nuevo_directorio)
+    return arreglo_dir
+
+def _crear_carpetas(arr):
+    for item in arr:
+        if not os.path.exists(item):
+	        pathlib.Path(item).mkdir(parents=True, exist_ok=True)
+
+def _mover_archivos(x, y):
+    arr_log = []
+    count = 0
+    for i in range(len(x)):
+        shutil.copyfile(x[i], y[i])
+        count +=1
+        log = 'El archivo {} fue copiado.\n'.format(x[i])
+        arr_log.append(log)
+    with open('log.txt', 'w') as f:
+        for i in arr_log:
+            f.write(i)
+    return count
+
+def _switch_case(o):
+    if o == 'M':
+        ruta = _solicitar_ruta_entrada()
+        lista_rutas = _convertir_archivo_en_lista(ruta)
+        lista_rutas_salida = list(lista_rutas)
+        path = _solicitar_path()
+        lista_rutas_salida = _rutas_digester(lista_rutas_salida, path)
+        lista_directorios = _crear_directorios(lista_rutas_salida)
+        _crear_carpetas(lista_directorios)
+        count = _mover_archivos(lista_rutas, lista_rutas_salida)
+        print('Tarea finalizada. Se han copiado {} archivos.'.format(str(count)))
+    elif o == 'S':
+        sys.exit()
+    else:
+        print('Opción incorrecta.')
+
+if __name__ == '__main__':
+    while True:
+        _menu()
